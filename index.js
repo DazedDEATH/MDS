a0364a797fd2398e2c34b6beb55a4e052f694547
DazedDEATH <juan.rivera.marin@correounivalle.edu.co>
2022-11-16 11:29:53 -0500

    cambio_rivera

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express()
 //Definicion del puerto, donde se manipulan las variables de ambiente
 const port = process.env.PORT || 3000;
 
+
 //Primer Commit de la rama de Rivera
+//First commit Mendez
+
+//Cambio Final para las ramas de Rivera y Mendez
 
 //Get
 app.get('/master',(req,res)=>{
